45d83aaba0a3b6e8d1e724d1695498cbd75c31d2
Mark Liu <takemmitsutoru@gmail.com>
2011-10-12 17:33:55 +0000

    just a demo

---
+++ huang.cpp
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  huang.cpp
+ *
+ *    Description:  a little huang
+ *
+ *        Version:  1.0
+ *        Created:  10/12/2011 05:33:19 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Just a test";
+}
