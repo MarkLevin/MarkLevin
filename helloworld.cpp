555d4fb6968153f6351145ef52558666d1645cf3
Mark Liu <takemmitsutoru@gmail.com>
2011-10-12 17:22:30 +0000

    fuck

---
+++ helloworld.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helloworld.cpp
+ *
+ *    Description:  fuck
+ *
+ *        Version:  1.0
+ *        Created:  10/12/2011 05:20:52 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout>>"Hello";
+}
